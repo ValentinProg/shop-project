fa313f746bc3c528d320146cced93d0fb2283cde
Valentin <V.Kovalenko.prog@gmail.com>
2023-02-20 12:44:13 +0200

    changed some styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,24 @@ import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./Navbar.css";
 import { useStore } from "../store.js";
-
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   const getTotalAmount = useStore((state) => state.getTotalAmount);
   const cartItems = useStore((state) => state.cartItems);
   const totalAmount = useStore((state) => state.totalAmount);
- 
+
   useEffect(() => {
     getTotalAmount();
   }, [cartItems]);
 
-
   return (
     <div className="navbar">
+      <Link to="/" className="navbarLogo">
+        <p>YB</p>
+        <img src={logo} />
+        <p>MS</p>
+      </Link>
       <div className="links">
         <Link to="/">Shop</Link>
         <Link to="/cart">
